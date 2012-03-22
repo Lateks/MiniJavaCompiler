223746fd0608e5c920ccc31605f0934eb6063e24
Laura Leppanen <laura.leppanen@iki.fi>
2012-03-22 23:15:05 +0200

    Sketch an AST and begin parser implementation.

---
+++ MiniJavaCompiler/AbstractSyntaxTree.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniJavaCompiler
+{
+    public interface SyntaxTreeNode { }
+
+    public interface Statement : SyntaxTreeNode { }
+
+    public interface Expression : SyntaxTreeNode { }
+
+    public class SyntaxElement : SyntaxTreeNode
+    {
+        public int Row
+        {
+            get;
+            private set;
+        }
+        public int Col
+        {
+            get;
+            private set;
+        }
+
+        public SyntaxElement(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+    }
+
+    public class Program : SyntaxTreeNode
+    {
+        public MainClassDeclaration MainClass
+        {
+            get;
+            private set;
+        }
+        public List<ClassDeclaration> Classes
+        {
+            get;
+            private set;
+        }
+
+        public Program(MainClassDeclaration main_class,
+                       List<ClassDeclaration> class_declarations)
+        {
+            MainClass = main_class;
+            Classes = class_declarations;
+        }
+    }
+
+    public class ClassDeclaration : SyntaxElement
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+        public string InheritedClass
+        {
+            get;
+            private set;
+        }
+        public List<Declaration> Declarations
+        {
+            get;
+            private set;
+        }
+
+        public ClassDeclaration(string name, string inherited,
+            List<Declaration> declarations, int row, int col)
+            : base(row, col) { }
+    }
+
+    public class MainClassDeclaration : SyntaxElement
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+        public List<Statement> MainMethod
+        {
+            get;
+            private set;
+        }
+
+        public MainClassDeclaration(string name, List<Statement> mainMethod,
+            int row, int col) : base(row, col)
+        {
+            Name = name;
+            MainMethod = mainMethod;
+        }
+    }
+
+    public abstract class Declaration : SyntaxElement
+    {
+        public string Name
+        {
+            get;
+            private set;
+        }
+        public string Type
+        {
+            get;
+            private set;
+        }
+
+        public Declaration(string name, string type, int row, int col)
+            : base(row, col)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+
+    public class MethodDeclaration : Declaration
+    {
+        public List<VariableDeclaration> Formals
+        {
+            get;
+            private set;
+        }
+        public List<Statement> MethodBody
+        {
+            get;
+            private set;
+        }
+
+        public MethodDeclaration(string name, string type,
+            List<VariableDeclaration> formals, List<Statement> methodBody,
+            int row, int col)
+            : base(name, type, row, col)
+        {
+            Formals = formals;
+            MethodBody = methodBody;
+        }
+    }
+
+    public class VariableDeclaration : Declaration, Statement
+    {
+        public VariableDeclaration(string name, string type, int row, int col)
+            : base(name, type, row, col) { }
+    }
+
+    public class PrintStatement : SyntaxElement, Statement
+    {
+        public Expression Expression
+        {
+            get;
+            private set;
+        }
+
+        public PrintStatement(Expression expression, int row, int col)
+            : base(row, col)
+        {
+            Expression = expression;
+        }
+    }
+
+    public class ReturnStatement : SyntaxElement, Statement
+    {
+        public Expression Expression
+        {
+            get;
+            private set;
+        }
+
+        public ReturnStatement(Expression expression, int row, int col)
+            : base(row, col)
+        {
+            Expression = expression;
+        }
+    }
+
+    public class BlockStatement : SyntaxElement, Statement
+    {
+        public List<Statement> Statements
+        {
+            get;
+            private set;
+        }
+
+        public BlockStatement(List<Statement> statements, int row, int col)
+            : base(row, col)
+        {
+            Statements = statements;
+        }
+    }
+
+    public class AssertStatement : SyntaxElement, Statement
+    {
+        public Expression Expression
+        {
+            get;
+            private set;
+        }
+
+        public AssertStatement(Expression expression, int row, int col)
+            : base(row, col)
+        {
+            Expression = expression;
+        }
+    }
+
+    public class AssignmentStatement : SyntaxElement, Statement
+    {
+        public Expression LHS
+        {
+            get;
+            private set;
+        }
+        public Expression RHS
+        {
+            get;
+            private set;
+        }
+
+        public AssignmentStatement(Expression lhs, Expression rhs, int row, int col)
+            : base(row, col)
+        {
+            LHS = lhs;
+            RHS = rhs;
+        }
+    }
+
+    public class IfStatement : SyntaxElement, Statement
+    {
+        public Expression BooleanExpression
+        {
+            get;
+            private set;
+        }
+        public Statement Then
+        {
+            get;
+            private set;
+        }
+        public Statement Else
+        {
+            get;
+            private set;
+        }
+
+        public IfStatement(Expression booleanExp, Statement thenBranch,
+            Statement elseBranch, int row, int col)
+            : base(row, col)
+        {
+            BooleanExpression = booleanExp;
+            Then = thenBranch;
+            Else = elseBranch;
+        }
+    }
+
+    public class MethodInvocation : SyntaxElement, Statement, Expression
+    {
+        public Expression MethodOwner
+        {
+            get;
+            private set;
+        }
+        public string MethodName
+        {
+            get;
+            private set;
+        }
+        public List<Expression> CallParameters
+        {
+            get;
+            private set;
+        }
+
+        public MethodInvocation(Expression methodOwner, string methodName,
+            List<Expression> callParameters, int row, int col)
+            : base(row, col)
+        {
+            MethodOwner = methodOwner;
+            MethodName = methodName;
+            CallParameters = callParameters;
+        }
+    }
+
+    public class InstanceCreation : SyntaxElement, Expression
+    {
+        public string Type
+        {
+            get;
+            private set;
+        }
+
+        public InstanceCreation(string type, int row, int col)
+            : base(row, col)
+        {
+            Type = type;
+        }
+    }
+
+    public class UnaryNot : SyntaxElement, Expression
+    {
+        public Expression BooleanExpression
+        {
+            get;
+            private set;
+        }
+
+        public UnaryNot(Expression booleanExp, int row, int col)
+            : base(row, col)
+        {
+            BooleanExpression = booleanExp;
+        }
+    }
+
+    public abstract class BinaryOperator : SyntaxElement, Expression
+    {
+        public Expression LHS
+        {
+            get;
+            private set;
+        }
+        public Expression RHS
+        {
+            get;
+            private set;
+        }
+
+        public BinaryOperator(Expression lhs, Expression rhs, int row, int col)
+            : base(row, col)
+        {
+            LHS = lhs;
+            RHS = rhs;
+        }
+    }
+
+    public class ArithmeticOp : BinaryOperator
+    {
+        public ArithmeticOp(Expression lhs, Expression rhs, int row, int col)
+            : base(lhs, rhs, row, col) { }
+    }
+
+    public class LogicalOp : BinaryOperator
+    {
+        public LogicalOp(Expression lhs, Expression rhs, int row, int col)
+            : base(lhs, rhs, row, col) { }
+    }
+
+    public class BooleanExpression : SyntaxElement, Expression
+    {
+        public bool Value
+        {
+            get;
+            private set;
+        }
+
+        public BooleanExpression(bool value, int row, int col)
+            : base(row, col)
+        {
+            Value = value;
+        }
+    }
+
+    public class ThisExpression : SyntaxElement, Expression
+    {
+        public ThisExpression(int row, int col)
+            : base(row, col) { }
+    }
+
+    public class ArrayIndexExpression : SyntaxElement, Expression
+    {
+        public Expression Array
+        {
+            get;
+            private set;
+        }
+        public Expression Index
+        {
+            get;
+            private set;
+        }
+
+        public ArrayIndexExpression(Expression arrayReference,
+            Expression arrayIndex, int row, int col)
+            : base(row, col)
+        {
+            Array = arrayReference;
+            Index = arrayIndex;
+        }
+    }
+}
---
+++ MiniJavaCompiler/Parser.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiniJavaCompiler.LexicalAnalysis;
+using MiniJavaCompiler.Support.TokenTypes;
+
+namespace MiniJavaCompiler
+{
+    namespace SyntaxAnalysis
+    {
+        public class Parser
+        {
+            private Scanner scanner;
+            private Token input_token;
+
+            public Parser(Scanner scanner)
+            {
+                this.scanner = scanner;
+                this.input_token = null;
+            }
+
+            public Program Parse()
+            {
+                this.input_token = scanner.NextToken();
+                return Program();
+            }
+
+            private Program Program()
+            {
+                var main = MainClass();
+                var declarations = ClassDeclarationList();
+                return new Program(main, declarations);
+            }
+
+            private MainClassDeclaration MainClass()
+            {
+                Token startToken = Match<KeywordToken>("class");
+                Identifier classIdent = Match<Identifier>();
+                Match<LeftCurlyBrace>();
+                Match<KeywordToken>("public");
+                Match<KeywordToken>("static");
+                Match<MiniJavaType>("void");
+                Match<KeywordToken>("main");
+                Match<LeftParenthesis>();
+                Match<RightParenthesis>();
+                Match<LeftCurlyBrace>();
+                List<Statement> main_statements = StatementList();
+                Match<RightCurlyBrace>();
+                Match<RightCurlyBrace>();
+                return new MainClassDeclaration(classIdent.Value,
+                    main_statements, startToken.Row, startToken.Col);
+            }
+
+            private List<ClassDeclaration> ClassDeclarationList()
+            {
+                var classes = new List<ClassDeclaration>();
+                if (MatchWithoutAdvancing<KeywordToken>("public"))
+                { // produce an empty list (epsilon) otherwise
+                    classes.Add(ClassDeclaration());
+                    classes.AddRange(ClassDeclarationList());
+                }
+                return classes;
+            }
+
+            private List<Declaration> DeclarationList()
+            {
+                return NodeList<Declaration, RightCurlyBrace>(Declaration);
+            }
+
+            private List<Statement> StatementList()
+            {
+                return NodeList<Statement, RightCurlyBrace>(Statement);
+            }
+
+            private List<NodeType> NodeList<NodeType, FollowToken>(Func<NodeType> ParseNode)
+                where NodeType : SyntaxTreeNode
+                where FollowToken : Token
+            {
+                var nodeList = new List<NodeType>();
+                if (!MatchWithoutAdvancing<FollowToken>())
+                {
+                    nodeList.Add(ParseNode());
+                    nodeList.AddRange(NodeList<NodeType, FollowToken>(ParseNode));
+                }
+                return nodeList;
+            }
+
+            private Statement Statement()
+            {
+                throw new NotImplementedException();
+            }
+
+            private ClassDeclaration ClassDeclaration()
+            {
+                Token startToken = Match<KeywordToken>("class");
+                Identifier classIdent = Match<Identifier>();
+                string inheritedClass = OptionalInheritance();
+                Match<LeftCurlyBrace>();
+                List<Declaration> declarations = DeclarationList();
+                Match<RightCurlyBrace>();
+                return new ClassDeclaration(classIdent.Value, inheritedClass,
+                    declarations, startToken.Row, startToken.Col);
+            }
+
+            private Declaration Declaration()
+            {
+                throw new NotImplementedException();
+            }
+
+            private string OptionalInheritance()
+            {
+                if (MatchWithoutAdvancing<KeywordToken>("extends"))
+                {
+                    Match<KeywordToken>("extends");
+                    return Match<Identifier>().Value;
+                }
+                return null;
+            }
+
+            private bool MatchWithoutAdvancing<T>(string value = null) where T : Token
+            {
+                if (input_token is T &&
+                    (value == null ||
+                    ((StringToken)input_token).Value == value))
+                    return true;
+                return false;
+            }
+
+            private T Match<T>(string value = null) where T : Token
+            {
+                if (input_token is T)
+                {
+                    if (value == null ||
+                        ((StringToken)input_token).Value == value)
+                    {
+                        var temp = (T)input_token;
+                        input_token = scanner.NextToken();
+                        return temp;
+                    }
+                    else
+                    {
+                        // return an error node or throw an exception?
+                        throw new NotImplementedException();
+                    }
+                }
+                else if (input_token is ErrorToken)
+                {
+                    // return an error node or throw an exception?
+                    // some recovery needs to be done at this point
+                    throw new NotImplementedException();
+                }
+                else
+                {
+                    // return an error node or throw an exception?
+                    throw new NotImplementedException();
+                }
+            }
+        }
+    }
+}
