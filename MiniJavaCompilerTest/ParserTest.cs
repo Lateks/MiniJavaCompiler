cab0e5cc1ccd9980ec79d045e4e57d1593ba5362
Laura Leppanen <laura.leppanen@iki.fi>
2012-03-29 17:29:58 +0300

    Refactor test setups.

--- MiniJavaCompilerTest/ParserTest.cs
+++ MiniJavaCompilerTest/ParserTest.cs
@@ -42,19 +42,8 @@ namespace MiniJavaCompilerTest
         [Test]
         public void SimpleMainClassWithEmptyMainMethod()
         {
-            programTokens.Enqueue(new KeywordToken("class", 0, 0));
-            programTokens.Enqueue(new Identifier("ThisIsTheMainClass", 0, 0));
-            programTokens.Enqueue(new LeftCurlyBrace(0, 0));
-            programTokens.Enqueue(new KeywordToken("public", 0, 0));
-            programTokens.Enqueue(new KeywordToken("static", 0, 0));
-            programTokens.Enqueue(new MiniJavaType("void", 0, 0));
-            programTokens.Enqueue(new KeywordToken("main", 0, 0));
-            programTokens.Enqueue(new LeftParenthesis(0, 0));
-            programTokens.Enqueue(new RightParenthesis(0, 0));
-            programTokens.Enqueue(new LeftCurlyBrace(0, 0));
-            programTokens.Enqueue(new RightCurlyBrace(0, 0));
-            programTokens.Enqueue(new RightCurlyBrace(0, 0));
-            programTokens.Enqueue(new EOF(0, 0));
+            DeclareMainClassUntilMainMethod("ThisIsTheMainClass");
+            CloseMainMethodAndClass();
 
             var scanner = new StubScanner(programTokens);
             var parser = new Parser(scanner);
@@ -67,42 +56,11 @@ namespace MiniJavaCompilerTest
         [Test]
         public void SimpleMainClassWithArithmeticAndPrinting()
         {
-            programTokens.Enqueue(new KeywordToken("class", 0, 0));
-            programTokens.Enqueue(new Identifier("ThisIsTheMainClass", 0, 0));
-            programTokens.Enqueue(new LeftCurlyBrace(0, 0));
-            programTokens.Enqueue(new KeywordToken("public", 0, 0));
-            programTokens.Enqueue(new KeywordToken("static", 0, 0));
-            programTokens.Enqueue(new MiniJavaType("void", 0, 0));
-            programTokens.Enqueue(new KeywordToken("main", 0, 0));
-            programTokens.Enqueue(new LeftParenthesis(0, 0));
-            programTokens.Enqueue(new RightParenthesis(0, 0));
-            programTokens.Enqueue(new LeftCurlyBrace(0, 0));
-
-            // Declare a variable.
-            programTokens.Enqueue(new MiniJavaType("int", 0, 0));
-            programTokens.Enqueue(new Identifier("foo", 0, 0));
-            programTokens.Enqueue(new EndLine(0, 0));
-
-            // Assign value to variable.
-            programTokens.Enqueue(new Identifier("foo", 0, 0));
-            programTokens.Enqueue(new AssignmentToken(0, 0));
-            programTokens.Enqueue(new IntegerLiteralToken("42", 0, 0));
-            programTokens.Enqueue(new EndLine(0, 0));
-
-            // Print variable value.
-            programTokens.Enqueue(new KeywordToken("System", 0, 0));
-            programTokens.Enqueue(new MethodInvocationToken(0, 0));
-            programTokens.Enqueue(new KeywordToken("out", 0, 0));
-            programTokens.Enqueue(new MethodInvocationToken(0, 0));
-            programTokens.Enqueue(new KeywordToken("println", 0, 0));
-            programTokens.Enqueue(new LeftParenthesis(0, 0));
-            programTokens.Enqueue(new Identifier("foo", 0, 0));
-            programTokens.Enqueue(new RightParenthesis(0, 0));
-            programTokens.Enqueue(new EndLine(0, 0));
-
-            programTokens.Enqueue(new RightCurlyBrace(0, 0));
-            programTokens.Enqueue(new RightCurlyBrace(0, 0));
-            programTokens.Enqueue(new EOF(0, 0));
+            DeclareMainClassUntilMainMethod("ThisIsTheMainClass");
+            DeclareBasicVariable("foo", "int");
+            AssignIntegerToVariable("foo", "42");
+            PrintVariableValue("foo");
+            CloseMainMethodAndClass();
 
             var scanner = new StubScanner(programTokens);
             var programTree = new Parser(scanner).Parse();
@@ -127,40 +85,14 @@ namespace MiniJavaCompilerTest
         [Test]
         public void CreatingABaseTypeArray()
         {
-            programTokens.Enqueue(new KeywordToken("class", 0, 0));
-            programTokens.Enqueue(new Identifier("ThisIsTheMainClass", 0, 0));
-            programTokens.Enqueue(new LeftCurlyBrace(0, 0));
-            programTokens.Enqueue(new KeywordToken("public", 0, 0));
-            programTokens.Enqueue(new KeywordToken("static", 0, 0));
-            programTokens.Enqueue(new MiniJavaType("void", 0, 0));
-            programTokens.Enqueue(new KeywordToken("main", 0, 0));
-            programTokens.Enqueue(new LeftParenthesis(0, 0));
-            programTokens.Enqueue(new RightParenthesis(0, 0));
-            programTokens.Enqueue(new LeftCurlyBrace(0, 0));
-
-            // Declare an array variable.
-            programTokens.Enqueue(new MiniJavaType("int", 0, 0));
-            programTokens.Enqueue(new LeftBracket(0, 0));
-            programTokens.Enqueue(new RightBracket(0, 0));
-            programTokens.Enqueue(new Identifier("foo", 0, 0));
-            programTokens.Enqueue(new EndLine(0, 0));
-
-            // Create an array and assign it to the variable.
-            programTokens.Enqueue(new Identifier("foo", 0, 0));
-            programTokens.Enqueue(new AssignmentToken(0, 0));
-            programTokens.Enqueue(new KeywordToken("new", 0, 0));
-            programTokens.Enqueue(new MiniJavaType("int", 0, 0));
-            programTokens.Enqueue(new LeftBracket(0, 0));
-            programTokens.Enqueue(new IntegerLiteralToken("10", 0, 0));
-            programTokens.Enqueue(new RightBracket(0, 0));
-            programTokens.Enqueue(new EndLine(0, 0));
-
-            programTokens.Enqueue(new RightCurlyBrace(0, 0));
-            programTokens.Enqueue(new RightCurlyBrace(0, 0));
-            programTokens.Enqueue(new EOF(0, 0));
+            DeclareMainClassUntilMainMethod("ThisIsTheMainClass");
+            DeclareBasicArrayVariable("foo", "int");
+            AssignNewArrayToVariable("foo", "int", "10");
+            CloseMainMethodAndClass();
 
             var scanner = new StubScanner(programTokens);
             var programTree = new Parser(scanner).Parse();
+
             Assert.That(programTree.Classes.Count, Is.EqualTo(0));
             var mainMethod = programTree.MainClass.MainMethod;
             Assert.That(mainMethod.Count, Is.EqualTo(2));
@@ -177,6 +109,76 @@ namespace MiniJavaCompilerTest
             Assert.That(newinstance.Type, Is.EqualTo("int"));
             Assert.That(newinstance.ArraySize, Is.InstanceOf<IntegerLiteral>());
             Assert.That(((IntegerLiteral)newinstance.ArraySize).Value, Is.EqualTo("10"));
+        }
+
+        private void CloseMainMethodAndClass()
+        {
+            programTokens.Enqueue(new RightCurlyBrace(0, 0));
+            programTokens.Enqueue(new RightCurlyBrace(0, 0));
+            programTokens.Enqueue(new EOF(0, 0));
+        }
+
+        private void PrintVariableValue(string variableName)
+        {
+            programTokens.Enqueue(new KeywordToken("System", 0, 0));
+            programTokens.Enqueue(new MethodInvocationToken(0, 0));
+            programTokens.Enqueue(new KeywordToken("out", 0, 0));
+            programTokens.Enqueue(new MethodInvocationToken(0, 0));
+            programTokens.Enqueue(new KeywordToken("println", 0, 0));
+            programTokens.Enqueue(new LeftParenthesis(0, 0));
+            programTokens.Enqueue(new Identifier(variableName, 0, 0));
+            programTokens.Enqueue(new RightParenthesis(0, 0));
+            programTokens.Enqueue(new EndLine(0, 0));
+        }
+
+        private void AssignIntegerToVariable(string variableName, string integerValue)
+        {
+            programTokens.Enqueue(new Identifier(variableName, 0, 0));
+            programTokens.Enqueue(new AssignmentToken(0, 0));
+            programTokens.Enqueue(new IntegerLiteralToken(integerValue, 0, 0));
+            programTokens.Enqueue(new EndLine(0, 0));
+        }
+
+        private void DeclareMainClassUntilMainMethod(string className)
+        {
+            programTokens.Enqueue(new KeywordToken("class", 0, 0));
+            programTokens.Enqueue(new Identifier(className, 0, 0));
+            programTokens.Enqueue(new LeftCurlyBrace(0, 0));
+            programTokens.Enqueue(new KeywordToken("public", 0, 0));
+            programTokens.Enqueue(new KeywordToken("static", 0, 0));
+            programTokens.Enqueue(new MiniJavaType("void", 0, 0));
+            programTokens.Enqueue(new KeywordToken("main", 0, 0));
+            programTokens.Enqueue(new LeftParenthesis(0, 0));
+            programTokens.Enqueue(new RightParenthesis(0, 0));
+            programTokens.Enqueue(new LeftCurlyBrace(0, 0));
+        }
+
+        private void DeclareBasicVariable(string name, string type)
+        {
+            programTokens.Enqueue(new MiniJavaType(type, 0, 0));
+            programTokens.Enqueue(new Identifier(name, 0, 0));
+            programTokens.Enqueue(new EndLine(0, 0));
+        }
+
+        private void AssignNewArrayToVariable(string variableName, string arrayType, string arraySize)
+        {
+            programTokens.Enqueue(new Identifier(variableName, 0, 0));
+            programTokens.Enqueue(new AssignmentToken(0, 0));
+            programTokens.Enqueue(new KeywordToken("new", 0, 0));
+            programTokens.Enqueue(new MiniJavaType(arrayType, 0, 0));
+            programTokens.Enqueue(new LeftBracket(0, 0));
+            programTokens.Enqueue(new IntegerLiteralToken(arraySize, 0, 0));
+            programTokens.Enqueue(new RightBracket(0, 0));
+            programTokens.Enqueue(new EndLine(0, 0));
+        }
+
+        private void DeclareBasicArrayVariable(string name, string type)
+        {
+            programTokens.Enqueue(new MiniJavaType(type, 0, 0));
+            programTokens.Enqueue(new LeftBracket(0, 0));
+            programTokens.Enqueue(new RightBracket(0, 0));
+            programTokens.Enqueue(new Identifier(name, 0, 0));
+            programTokens.Enqueue(new EndLine(0, 0));
         }
     }
 }
