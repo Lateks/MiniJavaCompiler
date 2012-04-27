51df01557e8c054da5c73ee3949cf92e08ccf6c4
Laura Leppanen <laura.leppanen@iki.fi>
2012-04-27 17:12:38 +0300

    Add visitor stubs for semantic analysis.

---
+++ MiniJavaCompiler/NodeVisitor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiniJavaCompiler.AbstractSyntaxTree;
+
+namespace MiniJavaCompiler
+{
+    namespace SemanticAnalysis
+    {
+        public interface NodeVisitor
+        {
+            void visit(Program node);
+            void visit(ClassDeclaration node);
+            void visit(MainClassDeclaration node);
+            void visit(VariableDeclaration node);
+            void visit(MethodDeclaration node);
+            void visit(PrintStatement node);
+            void visit(ReturnStatement node);
+            void visit(BlockStatement node);
+            void visit(AssertStatement node);
+            void visit(AssignmentStatement node);
+            void visit(IfStatement node);
+            void visit(WhileStatement node);
+            void visit(MethodInvocation node);
+            void visit(InstanceCreationExpression node);
+            void visit(UnaryNotExpression node);
+            void visit(ArithmeticOpExpression node);
+            void visit(LogicalOpExpression node);
+            void visit(BooleanLiteralExpression node);
+            void visit(ThisExpression node);
+            void visit(ArrayIndexingExpression node);
+            void visit(VariableReferenceExpression node);
+            void visit(IntegerLiteralExpression node);
+        }
+    }
+}
