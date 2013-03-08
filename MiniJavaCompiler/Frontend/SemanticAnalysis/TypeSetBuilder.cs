e4182f5b5b2d79aedc0ee988dc45f48264e72e57
Laura Leppänen <laura.leppanen@iki.fi>
2013-03-08 21:53:58 +0200

    Simplify typeset building.

--- MiniJavaCompiler/Frontend/SemanticAnalysis/TypeSetBuilder.cs
+++ MiniJavaCompiler/Frontend/SemanticAnalysis/TypeSetBuilder.cs
@@ -5,35 +5,44 @@ using System;
 
 namespace MiniJavaCompiler.Frontend.SemanticAnalysis
 {
-    public class TypeSetBuilder : INodeVisitor
+    public class TypeSetBuilder
     {
-        private readonly Program _syntaxTree;
+        private readonly Program _root;
         private readonly List<string> _types;
         private readonly IErrorReporter _errorReporter;
-        private bool _errorsFound;
 
         public TypeSetBuilder(Program node, IErrorReporter errorReporter)
         {
-            _syntaxTree = node;
+            _root = node;
             _errorReporter = errorReporter;
             _types = new List<string>();
-            _errorsFound = false;
         }
 
         public IEnumerable<string> BuildTypeSet()
         {
-            _syntaxTree.Accept(this);
-            if (_errorsFound)
+            bool typesOk = Handle(_root.MainClass);
+            foreach (var classDecl in _root.Classes)
+            {
+                typesOk &= Handle(classDecl);
+            }
+
+            if (!typesOk)
             {
                 throw new CompilationError();
             }
             return _types;
         }
 
-        private void ReportConflict(string typeName, int row, int col)
+        public bool Handle(ClassDeclaration node)
         {
-            _errorReporter.ReportError(String.Format("Conflicting definitions for {0}.", typeName), row, col);
-            _errorsFound = true;
+            if (NameAlreadyDefined(node.Name))
+            {
+                _errorReporter.ReportError(String.Format("Conflicting definitions for {0}.",
+                    node.Name), node.Row, node.Col);
+                return false;
+            }
+            _types.Add(node.Name);
+            return true;
         }
 
         private bool NameAlreadyDefined(string name)
@@ -41,60 +50,5 @@ namespace MiniJavaCompiler.Frontend.SemanticAnalysis
             return _types.Contains(name) || MiniJavaInfo.IsBuiltInType(name);
         }
 
-        public void Visit(Program node) { }
-
-        public void Visit(ClassDeclaration node)
-        {
-            if (NameAlreadyDefined(node.Name))
-            {
-                ReportConflict(node.Name, node.Row, node.Col);
-            }
-            else
-            {
-                _types.Add(node.Name);
-            }
-        }
-
-        public void Visit(VariableDeclaration node) { }
-
-        public void Visit(MethodDeclaration node) { }
-
-        public void Visit(PrintStatement node) { }
-
-        public void Visit(ReturnStatement node) { }
-
-        public void Visit(BlockStatement node) { }
-
-        public void Visit(AssertStatement node) { }
-
-        public void Visit(AssignmentStatement node) { }
-
-        public void Visit(IfStatement node) { }
-
-        public void Visit(WhileStatement node) { }
-
-        public void Visit(MethodInvocation node) { }
-
-        public void Visit(InstanceCreationExpression node) { }
-
-        public void Visit(UnaryOperatorExpression node) { }
-
-        public void Visit(BinaryOperatorExpression node) { }
-
-        public void Visit(BooleanLiteralExpression node) { }
-
-        public void Visit(ThisExpression node) { }
-
-        public void Visit(ArrayIndexingExpression node) { }
-
-        public void Visit(VariableReferenceExpression node) { }
-
-        public void Visit(IntegerLiteralExpression node) { }
-
-        public void Exit(ClassDeclaration node) { }
-
-        public void Exit(MethodDeclaration node) { }
-
-        public void Exit(BlockStatement node) { }
     }
 }
