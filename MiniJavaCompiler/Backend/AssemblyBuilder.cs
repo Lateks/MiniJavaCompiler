ba41488422f4977955868ffae4cad2c231fa3de1
Laura Leppänen <laura.leppanen@iki.fi>
2013-03-08 21:41:35 +0200

    Re-encapsulate all functionality relevant to code generation inside CodeGenerator.

--- MiniJavaCompiler/Backend/AssemblyBuilder.cs
+++ MiniJavaCompiler/Backend/AssemblyBuilder.cs
@@ -4,220 +4,166 @@ using MiniJavaCompiler.Support.SymbolTable;
 using MiniJavaCompiler.Support.SymbolTable.Symbols;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 
 namespace MiniJavaCompiler.Backend
 {
-    public struct AssemblyData
+    public partial class CodeGenerator
     {
-        public AssemblyBuilder AsmBuilder;
-        public ModuleBuilder ModBuilder;
-        public Dictionary<Type, ConstructorInfo> Constructors;
-
-        public AssemblyData(AssemblyBuilder asmBuilder, ModuleBuilder modBuilder,
-            Dictionary<Type, ConstructorInfo> constructors)
+        private class AssemblyCreator : INodeVisitor
         {
-            AsmBuilder = asmBuilder;
-            ModBuilder = modBuilder;
-            Constructors = constructors;
-        }
-    }
+            private CodeGenerator _parent;
+            private TypeBuilder _currentType;
+            private MethodBuilder _currentMethod;
 
-    public class AssemblyCreator : INodeVisitor
-    {
-        private readonly string _moduleName;
-        private readonly Program _astRoot;
-        private readonly SymbolTable _symbolTable;
-        private readonly Dictionary<Type, ConstructorInfo> _constructors;
-
-        private TypeBuilder _currentType;
-        private MethodBuilder _currentMethod;
-        private AssemblyBuilder _asmBuilder;
-        private ModuleBuilder _moduleBuilder;
-
-        public AssemblyCreator(SymbolTable symbolTable, Program abstractSyntaxTree, string moduleName)
-        {
-            _symbolTable = symbolTable;
-            _astRoot = abstractSyntaxTree;
-            _constructors = new Dictionary<Type, ConstructorInfo>();
-            _moduleName = moduleName;
-        }
-
-        // Set up a single module assembly as well as all user-defined types, methods and variables.
-        public AssemblyData SetUpAssembly()
-        {
-            AssemblyName name = new AssemblyName(_moduleName);
-            _asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(name, AssemblyBuilderAccess.Save);
-            _moduleBuilder = _asmBuilder.DefineDynamicModule(_moduleName);
-
-            SetUpScalarTypes();
-            _astRoot.Accept(this);
-
-            return new AssemblyData(_asmBuilder, _moduleBuilder, _constructors);
-        }
-
-        // Defines TypeBuilders for all user defined types and stores them and their constructors.
-        private void SetUpScalarTypes()
-        {
-            foreach (string typeName in _symbolTable.ScalarTypeNames)
+            public AssemblyCreator(CodeGenerator parent)
             {
-                TypeSymbol sym = _symbolTable.ResolveTypeName(typeName);
-                TypeBuilder typeBuilder = _moduleBuilder.DefineType(typeName, TypeAttributes.Public | TypeAttributes.Class); // TODO: are these IsByRef by default?
-                sym.Builder = typeBuilder;
-                _constructors[typeBuilder] =
-                    typeBuilder.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.Static);
-            }
-        }
-
-        public void Visit(Program node) { }
-
-        public void Visit(ClassDeclaration node)
-        {
-            TypeBuilder thisType = _symbolTable.ResolveTypeName(node.Name).Builder;
-            if (node.InheritedClass != null)
-            {
-                TypeBuilder superClass = _symbolTable.ResolveTypeName(node.InheritedClass).Builder;
-                thisType.SetParent(superClass);
-            }
-            _currentType = thisType;
-        }
-
-        public void Exit(ClassDeclaration node)
-        {
-            _currentType = null;
-        }
-
-        public void Visit(VariableDeclaration node)
-        {
-            switch (node.VariableKind)
-            {
-                case VariableDeclaration.Kind.Formal:
-                    _currentMethod.DefineParameter(GetParameterIndex(node), ParameterAttributes.In, node.Name); // TODO: is this builder still needed afterwards?
-                    break;
-                case VariableDeclaration.Kind.Local:
-                    _currentMethod.GetILGenerator().DeclareLocal(BuildType(node.Type, node.IsArray));
-                    break;
-                case VariableDeclaration.Kind.Class:
-                    _currentType.DefineField(node.Name, BuildType(node.Type, node.IsArray), FieldAttributes.Public);
-                    break;
-            }
-        }
-
-        // If the method is not static, parameter 0 is a reference to the object
-        // and this needs to be taken into account.
-        private int GetParameterIndex(VariableDeclaration node)
-        {
-            return _currentMethod.IsStatic ? node.LocalIndex : node.LocalIndex + 1;
-        }
-
-        public void Visit(MethodDeclaration node)
-        {
-            MethodBuilder methodBuilder = _currentType.DefineMethod(node.Name, GetMethodAttributes(node));
-            if (node.Name == MiniJavaInfo.MainMethodIdent)
-            {
-                _asmBuilder.SetEntryPoint(methodBuilder);
+                _parent = parent;
             }
 
-            methodBuilder.SetReturnType(GetReturnType(node));
-            methodBuilder.SetParameters(GetParameterTypes(node));
+            // Sets up a single module assembly as well as all user-defined types, methods and variables.
+            public void SetUpAssembly()
+            {
+                AssemblyName name = new AssemblyName(_parent._moduleName);
+                _parent._asmBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(name, AssemblyBuilderAccess.Save);
+                _parent._moduleBuilder = _parent._asmBuilder.DefineDynamicModule(_parent._moduleName);
 
-            _currentMethod = methodBuilder;
+                SetUpScalarTypes();
+                _parent._astRoot.Accept(this);
+            }
+
+            // Defines TypeBuilders for all user defined types and stores them and their constructors.
+            private void SetUpScalarTypes()
+            {
+                foreach (string typeName in _parent._symbolTable.ScalarTypeNames)
+                {
+                    TypeSymbol sym = _parent._symbolTable.ResolveTypeName(typeName);
+                    TypeBuilder typeBuilder = _parent._moduleBuilder.DefineType(
+                        typeName, TypeAttributes.Public | TypeAttributes.Class); // TODO: are these IsByRef by default?
+                    sym.Builder = typeBuilder;
+                    _parent._constructors[typeBuilder] =
+                        typeBuilder.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.Static);
+                }
+            }
+
+            public void Visit(Program node) { }
+
+            public void Visit(ClassDeclaration node)
+            {
+                TypeBuilder thisType = _parent._symbolTable.ResolveTypeName(node.Name).Builder;
+                if (node.InheritedClass != null)
+                {
+                    TypeBuilder superClass = _parent._symbolTable.ResolveTypeName(node.InheritedClass).Builder;
+                    thisType.SetParent(superClass);
+                }
+                _currentType = thisType;
+            }
+
+            public void Exit(ClassDeclaration node)
+            {
+                _currentType = null;
+            }
+
+            public void Visit(VariableDeclaration node)
+            {
+                switch (node.VariableKind)
+                {
+                    case VariableDeclaration.Kind.Formal:
+                        _currentMethod.DefineParameter(GetParameterIndex(node, _currentMethod),
+                            ParameterAttributes.In, node.Name); // TODO: is this builder still needed afterwards?
+                        break;
+                    case VariableDeclaration.Kind.Local:
+                        _currentMethod.GetILGenerator().DeclareLocal(
+                            _parent.BuildType(node.Type, node.IsArray));
+                        break;
+                    case VariableDeclaration.Kind.Class:
+                        _currentType.DefineField(node.Name,
+                            _parent.BuildType(node.Type, node.IsArray), FieldAttributes.Public);
+                        break;
+                }
+            }
+
+            public void Visit(MethodDeclaration node)
+            {
+                MethodBuilder methodBuilder = _currentType.DefineMethod(node.Name, GetMethodAttributes(node));
+                if (node.Name == MiniJavaInfo.MainMethodIdent)
+                {
+                    _parent._asmBuilder.SetEntryPoint(methodBuilder);
+                }
+
+                methodBuilder.SetReturnType(GetReturnType(node));
+                methodBuilder.SetParameters(GetParameterTypes(node));
+
+                _currentMethod = methodBuilder;
+            }
+
+            public void Exit(MethodDeclaration node)
+            {
+                _currentMethod = null;
+            }
+
+            private Type[] GetParameterTypes(MethodDeclaration node)
+            {
+                Type[] types = new Type[node.Formals.Count];
+                for (int i = 0; i < node.Formals.Count; i++)
+                {
+                    VariableDeclaration decl = node.Formals[i];
+                    types[i] = _parent.BuildType(decl.Type, decl.IsArray);
+                }
+                return types;
+            }
+
+            private Type GetReturnType(MethodDeclaration node)
+            {
+                MethodSymbol sym = _parent._symbolTable.ResolveClass(node).Scope.ResolveMethod(node.Name);
+                return _parent.BuildType(node.Type, node.IsArray);
+            }
+
+            private static MethodAttributes GetMethodAttributes(MethodDeclaration node)
+            {
+                MethodAttributes attrs = MethodAttributes.Public; // all methods are public
+                if (node.IsStatic)
+                {
+                    attrs |= MethodAttributes.Static;
+                }
+                return attrs;
+            }
+
+            public void Visit(PrintStatement node) { }
+
+            public void Visit(ReturnStatement node) { }
+
+            public void Visit(BlockStatement node) { }
+
+            public void Visit(AssertStatement node) { }
+
+            public void Visit(AssignmentStatement node) { }
+
+            public void Visit(IfStatement node) { }
+
+            public void Visit(WhileStatement node) { }
+
+            public void Visit(MethodInvocation node) { }
+
+            public void Visit(InstanceCreationExpression node) { }
+
+            public void Visit(UnaryOperatorExpression node) { }
+
+            public void Visit(BinaryOperatorExpression node) { }
+
+            public void Visit(BooleanLiteralExpression node) { }
+
+            public void Visit(ThisExpression node) { }
+
+            public void Visit(ArrayIndexingExpression node) { }
+
+            public void Visit(VariableReferenceExpression node) { }
+
+            public void Visit(IntegerLiteralExpression node) { }
+
+            public void Exit(BlockStatement node) { }
         }
-
-        public void Exit(MethodDeclaration node)
-        {
-            _currentMethod = null;
-        }
-
-        private Type[] GetParameterTypes(MethodDeclaration node)
-        {
-            Type[] types = new Type[node.Formals.Count];
-            for (int i = 0; i < node.Formals.Count; i++)
-            {
-                VariableDeclaration decl = node.Formals[i];
-                types[i] = BuildType(decl.Type, decl.IsArray);
-            }
-            return types;
-        }
-
-        private Type GetReturnType(MethodDeclaration node)
-        {
-            MethodSymbol sym = _symbolTable.ResolveClass(node).Scope.ResolveMethod(node.Name);
-            return BuildType(node.Type, node.IsArray);
-        }
-
-        private Type BuildType(string typeName, bool isArray)
-        {
-            Type type;
-            if (typeName == MiniJavaInfo.VoidType)
-            {
-                type = typeof(void);
-            }
-            if (typeName == MiniJavaInfo.IntType)
-            {
-                type = typeof(Int32);
-            }
-            else if (typeName == MiniJavaInfo.BoolType)
-            {
-                type = typeof(Boolean);
-            }
-            else
-            {
-                type = _symbolTable.ResolveTypeName(typeName).Builder;
-            }
-
-            if (isArray)
-            {
-                type = type.MakeArrayType();
-            }
-            return type;
-        }
-
-        private static MethodAttributes GetMethodAttributes(MethodDeclaration node)
-        {
-            MethodAttributes attrs = MethodAttributes.Public; // all methods are public
-            if (node.IsStatic)
-            {
-                attrs |= MethodAttributes.Static;
-            }
-            return attrs;
-        }
-
-        public void Visit(PrintStatement node) { }
-
-        public void Visit(ReturnStatement node) { }
-
-        public void Visit(BlockStatement node) { }
-
-        public void Visit(AssertStatement node) { }
-
-        public void Visit(AssignmentStatement node) { }
-
-        public void Visit(IfStatement node) { }
-
-        public void Visit(WhileStatement node) { }
-
-        public void Visit(MethodInvocation node) { }
-
-        public void Visit(InstanceCreationExpression node) { }
-
-        public void Visit(UnaryOperatorExpression node) { }
-
-        public void Visit(BinaryOperatorExpression node) { }
-
-        public void Visit(BooleanLiteralExpression node) { }
-
-        public void Visit(ThisExpression node) { }
-
-        public void Visit(ArrayIndexingExpression node) { }
-
-        public void Visit(VariableReferenceExpression node) { }
-
-        public void Visit(IntegerLiteralExpression node) { }
-
-        public void Exit(BlockStatement node) { }
     }
 }
---
+++ MiniJavaCompiler/Backend/InstructionGenerator.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MiniJavaCompiler.Support.SymbolTable;
+using MiniJavaCompiler.Support.AbstractSyntaxTree;
+using System.Reflection;
+using System.Reflection.Emit;
+using MiniJavaCompiler.Support.SymbolTable.Symbols;
+using MiniJavaCompiler.Support.SymbolTable.Types;
+using MiniJavaCompiler.Support;
+
+namespace MiniJavaCompiler.Backend
+{
+    public partial class CodeGenerator
+    {
+        private class InstructionGenerator : INodeVisitor
+        {
+            private CodeGenerator _parent;
+            private TypeBuilder _currentType;
+            private MethodBuilder _currentMethod;
+
+            private static Dictionary<MiniJavaInfo.Operator, OpCode> operators =
+                new Dictionary<MiniJavaInfo.Operator, OpCode>()
+            {
+                { MiniJavaInfo.Operator.Add, OpCodes.Add },
+                { MiniJavaInfo.Operator.Sub, OpCodes.Sub },
+                { MiniJavaInfo.Operator.Div, OpCodes.Div },
+                { MiniJavaInfo.Operator.Mul, OpCodes.Mul },
+                { MiniJavaInfo.Operator.Lt, OpCodes.Clt },
+                { MiniJavaInfo.Operator.Gt, OpCodes.Cgt },
+                { MiniJavaInfo.Operator.And, OpCodes.And },
+                { MiniJavaInfo.Operator.Or, OpCodes.Or },
+                { MiniJavaInfo.Operator.Eq, OpCodes.Ceq },
+                { MiniJavaInfo.Operator.Mod, OpCodes.Rem },
+                { MiniJavaInfo.Operator.Not, OpCodes.Not }
+            };
+
+            public InstructionGenerator(CodeGenerator parent)
+            {
+                _parent = parent;
+            }
+
+            public void GenerateInstructions()
+            {
+                _parent._astRoot.Accept(this);
+            }
+
+            public void Visit(Program node) { }
+
+            public void Visit(ClassDeclaration node)
+            {
+                TypeBuilder thisType = _parent._symbolTable.ResolveTypeName(node.Name).Builder;
+                _currentType = thisType;
+            }
+
+            public void Visit(VariableDeclaration node) { }
+
+            public void Visit(MethodDeclaration node)
+            {
+                _currentMethod = _parent._symbolTable.Scopes[node].ResolveMethod(node.Name).Builder;
+            }
+
+            public void Visit(PrintStatement node)
+            {
+                MethodInfo printMethod = typeof(System.Console).GetMethod("WriteLine", new Type[] { typeof(string) });
+                _currentMethod.GetILGenerator().Emit(OpCodes.Call, printMethod);
+            }
+
+            public void Visit(ReturnStatement node)
+            {
+                _currentMethod.GetILGenerator().Emit(OpCodes.Ret);
+            }
+
+            public void Visit(BlockStatement node)
+            {
+                _currentMethod.GetILGenerator().BeginScope();
+            }
+
+            public void Visit(AssertStatement node)
+            {
+                MethodInfo assertMethod = typeof(System.Diagnostics.Debug).GetMethod("Assert", new Type[] { typeof(bool) });
+                _currentMethod.GetILGenerator().Emit(OpCodes.Call, assertMethod);
+            }
+
+            public void Visit(AssignmentStatement node)
+            { // The left hand side of an assignment must be either
+                // a variable reference or an array indexing expression.
+                var il = _currentMethod.GetILGenerator();
+                if (node.LeftHandSide is VariableReferenceExpression)
+                {
+                    var reference = (VariableReferenceExpression)node.LeftHandSide;
+                    var variable = _parent._symbolTable.Scopes[reference].ResolveVariable(reference.Name);
+                    var decl = (VariableDeclaration)_parent._symbolTable.Definitions[variable];
+                    switch (decl.VariableKind)
+                    {
+                        case VariableDeclaration.Kind.Class:
+                            il.Emit(OpCodes.Stfld, decl.Name);
+                            break;
+                        case VariableDeclaration.Kind.Local:
+                            il.Emit(OpCodes.Stloc, decl.LocalIndex);
+                            break;
+                        case VariableDeclaration.Kind.Formal:
+                            il.Emit(OpCodes.Starg, GetParameterIndex(decl, _currentMethod));
+                            break;
+                    }
+                }
+                else
+                {
+                    // TODO: handle array case
+                    throw new NotImplementedException();
+                }
+            }
+
+            public void Visit(IfStatement node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Visit(WhileStatement node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Visit(MethodInvocation node)
+            {
+                MethodInfo method;
+                if (node.MethodOwner.Type is ArrayType)
+                {
+                    method = _parent.BuildType(node.MethodOwner.Type.Name, true).
+                        GetMethod("Length", new Type[] { });
+                }
+                else
+                {   // TODO: should there be a separate pass to define all method builders?
+                    method = _parent._symbolTable.ResolveTypeName(node.MethodOwner.Type.Name).
+                        Scope.ResolveMethod(node.MethodName).Builder;
+                }
+                _currentMethod.GetILGenerator().Emit(OpCodes.Call, method);
+            }
+
+            public void Visit(InstanceCreationExpression node)
+            {
+                // TODO: handle array creation cases
+                Type type = _parent.BuildType(node.CreatedType, node.IsArrayCreation);
+                _currentMethod.GetILGenerator().Emit(OpCodes.Newobj, _parent._constructors[type]);
+            }
+
+            public void Visit(UnaryOperatorExpression node)
+            {
+                _currentMethod.GetILGenerator().Emit(operators[node.Operator]);
+            }
+
+            public void Visit(BinaryOperatorExpression node)
+            {
+                _currentMethod.GetILGenerator().Emit(operators[node.Operator]);
+            }
+
+            public void Visit(BooleanLiteralExpression node)
+            {
+                _currentMethod.GetILGenerator().Emit(node.Value ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
+            }
+
+            public void Visit(ThisExpression node)
+            {
+                _currentMethod.GetILGenerator().Emit(OpCodes.Ldarg_0);
+            }
+
+            public void Visit(ArrayIndexingExpression node)
+            {
+                throw new NotImplementedException();
+            }
+
+            public void Visit(VariableReferenceExpression node)
+            {
+                var variable = _parent._symbolTable.Scopes[node].ResolveVariable(node.Name);
+                var definition = (VariableDeclaration)_parent._symbolTable.Definitions[variable];
+                var il = _currentMethod.GetILGenerator();
+                switch (definition.VariableKind)
+                {
+                    case VariableDeclaration.Kind.Class:
+                        il.Emit(OpCodes.Ldarg_0);
+                        il.Emit(OpCodes.Ldfld, node.Name);
+                        break;
+                    case VariableDeclaration.Kind.Formal:
+                        il.Emit(OpCodes.Ldarg, GetParameterIndex(definition, _currentMethod));
+                        break;
+                    case VariableDeclaration.Kind.Local:
+                        il.Emit(OpCodes.Ldloc, definition.LocalIndex);
+                        break;
+                }
+            }
+
+            public void Visit(IntegerLiteralExpression node)
+            {
+                _currentMethod.GetILGenerator().Emit(OpCodes.Ldc_I4, node.IntValue);
+            }
+
+            public void Exit(ClassDeclaration node)
+            {
+                _currentType.CreateType();
+                _currentType = null;
+            }
+
+            public void Exit(MethodDeclaration node)
+            {
+                _currentMethod = null;
+            }
+
+            public void Exit(BlockStatement node)
+            {
+                _currentMethod.GetILGenerator().EndScope();
+            }
+        }
+    }
+}
